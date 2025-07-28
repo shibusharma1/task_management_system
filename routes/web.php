5ac51c8f9867363c554cd4fbd664ba532410d75d
Shibu Sharma <168130059+shibusharma1@users.noreply.github.com>
2025-07-28 14:00:41 +0545

    test

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
+Route::get('login',[AuthController::class,'login']);
